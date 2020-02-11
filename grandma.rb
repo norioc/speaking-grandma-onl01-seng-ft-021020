2205ccc50359ce00af61e55b70b0053b5ebcc579
norio c <norionorioc@gmail.com>
2020-02-11 20:23:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -22,6 +22,9 @@ def speak_to_grandma(phrase)
     
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
+    
+  elsif phrase == "WHAT?"
+    return "NO, NOT SICNE"
   
   else
     return "HUH?! SPEAK UP, SONNY!"
